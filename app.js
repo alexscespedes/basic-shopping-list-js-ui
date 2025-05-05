2e443dcf0e076089181fd7ec96d2870268a27e65
alexscespedes <alexandersenc99@gmail.com>
2025-05-05 04:57:11 -0400

    add new item to the shopping list

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+// Get DOM Elements
+
+const itemInput = document.getElementById("itemInput");
+const totalItems = document.getElementById("itemCount");
+const shoppingList = document.getElementById("shoppingList");
+
+function addItem() {
+  const item = itemInput.value.trim();
+
+  if (item === "") {
+    alert("Please enter a valid item");
+  }
+
+  console.log(item);
+  addShoppingList(item);
+  //   totalItems.innerHTML = "Total Items 1";
+  //   event.preventDefault();
+}
+
+function addShoppingList(item) {
+  const itemList = document.createElement("li");
+
+  itemList.innerHTML = `
+  <li>${item}</li>
+  `;
+
+  shoppingList.appendChild(itemList);
+}
+
+// addEventListener("click", function (e) {
+
+// });
