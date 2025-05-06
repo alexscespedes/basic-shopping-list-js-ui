202879bb8820c1ed11f81410fcf6429c24685e36
alexscespedes <alexandersenc99@gmail.com>
2025-05-06 05:02:12 -0400

    add mark as purchased code

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ function addItem() {
 
   if (item === "") {
     alert("Please enter a valid item");
+    return;
   }
 
   console.log(item);
   addShoppingList(item);
+
+  // Clear input after submit.
+  itemInput.value = "";
   //   totalItems.innerHTML = "Total Items 1";
   //   event.preventDefault();
 }
@@ -21,9 +25,17 @@ function addShoppingList(item) {
   const itemList = document.createElement("li");
 
   itemList.innerHTML = `
-  <li>${item}</li>
-  <li><button class="delete-btn">Delete</button></li>
+  <li><span>${item}</span></li>
+  <li><span><button class="delete-btn">Delete</button></span></li>
   `;
+
+  /*
+It has to be a button that when is clicked, has to change name of the item (purchased)
+  //  <li class="purchased"><span>Purchase</span></li>
+
+  implement this just when the user select something like "purchase".
+  itemList.classList.toggle("purchased");
+  */
 
   itemList.querySelector(".delete-btn").addEventListener("click", function () {
     itemList.remove();
